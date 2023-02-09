1a49d568e6a479b16e02dbde60545d49583409c6
theArctesian <stephen.d.okita@gmail.com>
2023-02-09 20:01:08 +0800

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
         try {
             let num = Math.floor(Math.random() * questionLength);
             question = questions[num].Question;
-            questions.splice(num, 1); 
+            questions.splice(num, 1);
         } catch (error) {
             question = "you have exhausted all the questions, refresh the tab";
         }
@@ -18,27 +18,47 @@
 </script>
 
 <div class="card margin-auto text-center flex-col">
-    <div class="c">
-        <h1 class="text-center text-2xl"><b class="text-3xl">Question: </b><br /><br />{question}</h1>
-        <br />
-        <br />
-        <button class="but" on:click={genRandomQuestion}
-            >Get a new question</button
-        >
+    <div class="c flex flex-col justifty-between">
+        <div class="cc">
+            <h1 class="text-center m-auto text-2xl">
+                <b class="text-3xl">Question: </b><br />
+            </h1>
+        </div>
+        <div class="cc">
+            <Typewriter interval="10">
+                <h1>{question}</h1>
+            </Typewriter>
+        </div>
+        <div class="cc">
+            <button class="but" on:click={genRandomQuestion}
+                >Get a new question</button
+            >
+        </div>
+        <div class="cc">
+                <a href="/about"class="text-l underline a">About this website</a>
+        </div>
     </div>
 </div>
 
 <style>
+    .a{
+        color: var(--selection);
+    }
+    .cc {
+        margin: 1rem;
+    }
     .c {
         margin: auto;
+
+        justify-content: space-between;
         background-color: var(--fg);
         padding: 1rem;
-        border-radius: 1rem;
+        border-radius: 0.2rem;
     }
     .but {
         margin: auto;
         padding: 1rem;
-        border-radius: 1rem;
+        border-radius: 0.2rem;
         color: white;
         background-color: var(--bg);
         transition: all ease-in-out 200ms;
@@ -48,9 +68,8 @@
     }
     .card {
         display: flex;
-        height: fit-content;
+        height: 96vh;
         margin: 1rem;
-        background-color: var(--purple);
         padding: 1rem;
         border-radius: 1rem;
     }
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,162 @@
+<script>
+    import "../..//app.css";
+</script>
+
+<div class="wrapper">
+    <h1 class="text-center ">
+        Hello I made this website for fun you can find out who I am at my
+        website <a href="https://www.danielokita.com" class="underline link"
+            >danielokita.com</a
+        >, Email me at <span class="underline">me@danielokita.com</span>, or
+        find me on these places:
+    </h1>
+    <div class="text-center ">
+        <div class="content">
+            <div class="element">
+                <a href="https://www.linkedin.com/in/stephen-okita-a9427b1b2/">
+                    <img src="https://simpleicons.org/icons/linkedin.svg" />
+                </a>
+            </div>
+            <div class="element">
+                <a href="https://mastodon.world/@thearctesian">
+                    <img src="https://simpleicons.org/icons/mastodon.svg" />
+                </a>
+            </div>
+            <div class="element">
+                <a href="https://github.com/TheArctesian">
+                    <img src="https://simpleicons.org/icons/github.svg" />
+                </a>
+            </div>
+            <div class="element">
+                <a href="https://discord.com/users/682753580943278083">
+                    <img src="https://simpleicons.org/icons/discord.svg" />
+                </a>
+            </div>
+        </div>
+        <h1>
+            as well as the source code for this website <a
+                class="s"
+                href="https://github.com/TheArctesian/TableQuestions.net"
+            >
+                here</a
+            >
+        </h1>
+        <h1>
+            If you would like to donate to me you can find my eth links bellow
+        </h1>
+        <br />
+        <div class="donate flex flex-col">
+            <h1><b>Eth: </b>`0xc7AfE4114E3b78cB22Ec7EbDA11AD40199a9Cb96`</h1>
+            <h1>
+                <b
+                    >Cardano:
+                </b>`addr1q85kef4y4zx4lyxyuq3wgec3nddn53wv6nmydrc6eyx5l47jdatz0hja95dudtxclcjp8ejkthl6hl5xjfregk9lllrs8um6c0`
+            </h1>
+        </div>
+        <div >
+            <a href="https://ko-fi.com/N4N5ILAUX" target="_blank"
+                ><img
+class="flex w-full s"
+                    src="https://storage.ko-fi.com/cdn/kofi3.png?v=3"
+                    alt="Buy Me a Coffee at ko-fi.com"
+                /></a
+            >
+        </div>
+    </div>
+</div>
+
+<style>
+    .donate {
+        width: fit-content;
+        padding: 1rem;
+        background-color: var(--purple);
+        color: white;
+        inline-size: 80vw;
+        overflow: auto;
+        border-radius: 0.2rem;
+    }
+    a {
+        color: var(--comment);
+        transition: all ease-in-out 200ms;
+    }
+    .link {
+        font-style: italic;
+    }
+    .link:hover {
+        transition: all ease-in-out 200ms;
+        font-style: normal;
+    }
+    @media (max-width: 750px) {
+        a {
+            font-size: 3.8vw;
+        }
+    }
+
+    .projects {
+        margin: auto;
+        text-decoration: none;
+        font-size: 5vw;
+        transition: all ease-in-out 400ms;
+        color: var(--purple);
+    }
+
+    :global(body) {
+        background-color: var(--bg);
+        font-family: "Fira Code", monospace;
+        letter-spacing: 0.1em; /* Adjust as needed */
+    }
+    .wrapper {
+        margin-left: 5vw;
+        margin-right: 5vw;
+        margin-top: 5vh;
+        margin-bottom: 5vh;
+        height: 90vh;
+        font-size: small;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        padding: 1rem;
+        border-radius: 0.2rem;
+        background-color: var(--fg);
+    }
+    .content {
+        margin: 1rem;
+        display: flex;
+        justify-content: center;
+        border-radius: 2rem;
+    }
+    .element {
+        margin: 1rem;
+        border-radius: 0.2rem;
+        padding: 1rem;
+        border: 0.2rem solid var(--fg);
+    }
+    .element:hover {
+        border: 0.2rem solid var(--purple);
+    }
+
+    @media (max-width: 750px) {
+        .element {
+            margin: 0.6rem;
+            border-radius: 0rem;
+            padding: 0rem;
+
+            border: 0.2rem solid var(--fg);
+        }
+    }
+    .text {
+        background-color: var(--fg);
+        padding: 1rem;
+        border-radius: 1rem;
+    }
+    img {
+        transition: all ease-in-out 200ms;
+        width: 7.5vw;
+    }
+    .s{
+        padding: 1rem;
+        margin: auto;
+        width: 40vw;
+    }
+</style>
